2fcddae102c46971c3d0214079c135541690d8da
Kosynskyi <kosynskyi.vova@gmail.com>
2023-02-24 15:30:22 +0200

    add login form in LoginPage

--- dist/pages/ProfilePage/ProfilePage.jsx
+++ dist/pages/ProfilePage/ProfilePage.jsx
@@ -4,8 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
+const material_1 = require("@mui/material");
 const ProfilePage = () => {
-    return <div>ProfilePage</div>;
+    return (<material_1.Box sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+        }}>
+      <material_1.Typography variant="h2"> Вітаємо на Вашій приватній сторінці</material_1.Typography>
+    </material_1.Box>);
 };
 exports.default = ProfilePage;
 //# sourceMappingURL=ProfilePage.jsx.map
